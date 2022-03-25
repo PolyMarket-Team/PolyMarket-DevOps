59240c0df274625ca7bc511eec1071d566a006b8
shinwoo <sinwoo1225@gmail.com>
2022-03-25 20:54:52 +0900

    sample app 프로젝트 생성
    - spring boot app 2개 생성
    - react app 프로젝트 생성

---
+++ sample-apps/sample-api2/src/main/java/kr/polymarket/sampleapi2/controller/HelloController.java
@@ -0,0 +1,15 @@
+package kr.polymarket.sampleapi2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public Mono<String> hello2() {
+        return Mono.just("hello2");
+    }
+
+}
---
+++ sample-apps/sample-api2/src/test/java/kr/polymarket/sampleapi2/SampleApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.polymarket.sampleapi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApi2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
